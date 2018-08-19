22d3910ba412f313ea33f0fa447006d98d1961d2
cardosok <cesarcardoso954@gmail.com>
2018-08-19 19:14:59 -0300

    Inicia Projeto

---
+++ src/nomes/e/cidades/NomesECidades.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nomes.e.cidades;
+
+/**
+ *
+ * @author César
+ */
+public class NomesECidades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
